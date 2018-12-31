08a84ccdf6280e35f3d6b949ba0fb81668fc9583
username <mmiahny@gmail.com>
2018-12-31 10:09:48 -0500

    Parser Package completed

--- src/parser/UnitTestingStudentProfile.java
+++ src/parser/UnitTestingStudentProfile.java
@@ -1,8 +1,18 @@
 package parser;
-
+import org.testng.Assert;
 public class UnitTestingStudentProfile {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
-
+        try {
+            Student student = new Student();
+            student.firstName.toString();
+            Assert.assertEquals("Monir", "Miah", "not match");
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
+
+
+
+
